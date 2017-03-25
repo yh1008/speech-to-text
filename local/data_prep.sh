842ebd97f919351a367cf689c84a3d522fb6cc22
yh1008 <ye.hua@columbia.edu>
2017-03-25 17:53:23 -0400

    add utt2spk->spk2utt

--- local/data_prep.sh
+++ local/data_prep.sh
@@ -2,5 +2,9 @@
 
 cp -r ../wsj/s5/steps ./
 cp -r ../wsj/s5/utils ./
+
 ./audio_data_prep.py
 ./acoustic_data_prep.py
+
+../utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+../utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
