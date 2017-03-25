b386c0f9e348d7f09ba87e873dae39954d0f85d7
yh1008 <ye.hua@columbia.edu>
2017-03-25 18:14:30 -0400

    utils/fix_data_dir.sh data/train add sorting

--- local/data_prep.sh
+++ local/data_prep.sh
@@ -6,5 +6,10 @@ cp -r ../wsj/s5/utils ./
 ./audio_data_prep.py
 ./acoustic_data_prep.py
 
+utils/fix_data_dir.sh data/train
+
 ../utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
 ../utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+
+mkdir conf
+cp ../timit/s5/conf/mfcc.conf ./conf
