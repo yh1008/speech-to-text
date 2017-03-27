11850e0aa3a8fa473acbac55c052db30a5a6b644
yh1008 <ye.hua@columbia.edu>
2017-03-27 00:02:00 -0400

    add export LC_ALL=C sort using C style

--- local/data_prep.sh
+++ local/data_prep.sh
@@ -6,7 +6,9 @@ cp -r ../wsj/s5/utils ./
 ./audio_data_prep.py
 ./acoustic_data_prep.py
 
+export LC_ALL=C
 utils/fix_data_dir.sh data/train
+utils/fix_data_dir.sh data/test 
 
 ../utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
 ../utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
