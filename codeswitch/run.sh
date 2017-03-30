cf220a20fc45a9df4ab27c31827d221449a7a648
yh1008 <ye.hua@columbia.edu>
2017-03-30 16:17:16 -0400

    add nj variable

--- codeswitch/run.sh
+++ codeswitch/run.sh
@@ -2,13 +2,22 @@
 
 [ -f path.sh ] && . ./path.sh
 
+feats_nj=8
+
 echo ============================================================================
-echo "                MFCC Feature Extration                    "
+echo            "                Data Preperation                    "
 echo ============================================================================
+
+./local/data_prep.sh
+
+echo ============================================================================
+echo          "                MFCC Feature Extration                    "
+echo ============================================================================
+
 mkdir conf
 cp ../timit/s5/conf/mfcc.conf ./conf
-steps/make_mfcc.sh --nj 8 data/train exp/make_mfcc/train mfcc
-steps/make_mfcc.sh --nj 8 data/test exp/make_mfcc/test mfcc
+steps/make_mfcc.sh --nj $feats_nj data/train exp/make_mfcc/train mfcc
+steps/make_mfcc.sh --nj $feats_nj data/test exp/make_mfcc/test mfcc
 
 steps/compute_cmvn_stats.sh data/train exp/make_mfcc/train mfcc
 steps/compute_cmvn_stats.sh data/test exp/make_mfcc/test mfcc
