9797dab41a3abaa3f2767e673bc1ba27c68822de
yh1008 <ye.hua@columbia.edu>
2017-04-06 12:20:16 -0400

    fix where to execute utils

--- codeswitch/local/data_prep.sh
+++ codeswitch/local/data_prep.sh
@@ -22,13 +22,14 @@ chmod 755 audio_data_prep.py
 chmod 755 acoustic_data_prep.py
 ./acoustic_data_prep.py
 
-export LC_ALL=C
+export LC_ALL=C # set sorting mechanism 
 utils/fix_data_dir.sh data/train
 utils/fix_data_dir.sh data/test 
 
-# Make the utt2spk and spk2utt files.
-../utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
-../utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
+# Make the utt2spk and spk2utt files. Execute utils script under codeswitch directory NOT local 
+cd ../
+utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
+utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
 
 utils/fix_data_dir.sh data/train
 utils/fix_data_dir.sh data/test 
