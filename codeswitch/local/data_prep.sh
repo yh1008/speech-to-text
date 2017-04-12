e01a8a8230605845a97cdd9fe704e1a930e3ac92
yh1008 <ye.hua@columbia.edu>
2017-04-12 00:34:41 -0400

    change order of fixdirectory

--- codeswitch/local/data_prep.sh
+++ codeswitch/local/data_prep.sh
@@ -20,15 +20,12 @@ chmod 755 audio_data_prep.py
 chmod 755 acoustic_data_prep.py
 ./acoustic_data_prep.py
 
-export LC_ALL=C # set sorting mechanism 
-utils/fix_data_dir.sh data/train
-utils/fix_data_dir.sh data/test 
-
-# Make the utt2spk and spk2utt files. Execute utils script under codeswitch directory NOT local 
+# Execute utils script under codeswitch directory NOT local 
 cd ../
+# Make the utt2spk and spk2utt files.
 utils/utt2spk_to_spk2utt.pl data/train/utt2spk > data/train/spk2utt
 utils/utt2spk_to_spk2utt.pl data/test/utt2spk > data/test/spk2utt
-
+export LC_ALL=C # set sorting mechanism 
 utils/fix_data_dir.sh data/train
 utils/fix_data_dir.sh data/test 
 
