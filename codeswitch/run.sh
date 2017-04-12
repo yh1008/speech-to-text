426da17188533f87748ff4ae01cfb36bca274085
yh1008 <ye.hua@columbia.edu>
2017-04-12 00:23:31 -0400

    fix decode.config to decode.conf

--- codeswitch/run.sh
+++ codeswitch/run.sh
@@ -67,7 +67,7 @@ echo ===========================================================================
 steps/train_mono.sh --nj $mono_nj --cmd "$train_cmd" data/train data/lang exp/mono
 steps/align_si.sh --nj $nj --cmd "$train_cmd" data/train data/lang exp/mono exp/mono_ali
 #utils/mkgraph.sh --mono data/lang exp/mono exp/mono/graph
-#steps/decode.sh --config conf/decode.config --nj $mono_nj --cmd "$decode_cmd" exp/mono/graph data/test exp/mono/decode
+#steps/decode.sh --config conf/decode.conf --nj $mono_nj --cmd "$decode_cmd" exp/mono/graph data/test exp/mono/decode
 
 echo ============================================================================
 echo            "               First Triphone Pass Training                   "
@@ -75,14 +75,14 @@ echo ===========================================================================
 
 steps/train_deltas.sh --cmd "$train_cmd" 1000 11000 data/train data/lang exp/mono_ali exp/tri1
 utils/mkgraph.sh data/lang exp/tri1 exp/tri1/graph #decoding
-steps/decode.sh --config conf/decode.config --nj $tri_nj --cmd "$decode_cmd" exp/tri1/graph data/test exp/tri1/decode
+steps/decode.sh --config conf/decode.conf --nj $tri_nj --cmd "$decode_cmd" exp/tri1/graph data/test exp/tri1/decode
 
 echo ============================================================================
 echo            "               Tri2: LDA+MLLT                  "
 echo ============================================================================
 
 steps/align_si.sh --nj 16 --cmd "$train_cmd" --use-graphs true data/train data/lang exp/tri1 exp/tri1_ali
-steps/train_lda_mllt.sh --cmd "$train_cmd" --splice-opts "--left-context=3 --right-context=3" 2000 11000 data/train data/lang exp/tri1_ali exp/tri2b
+steps/train_lda_mllt.sh --cmd "$train_cmd" --splice-opts "--left-context=3 --right-context=3" 1000 11000 data/train data/lang exp/tri1_ali exp/tri2b
 utils/mkgraph.sh data/lang exp/tri2b exp/tri2b/graph 
 steps/decode.sh --config conf/decode.conf --nj $decode_nj --cmd "$decode_cmd" exp/tri2b/graph data/test exp/tri2b/decode
 
