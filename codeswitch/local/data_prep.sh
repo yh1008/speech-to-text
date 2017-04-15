19774b139d87940e70e9193fd334ce3316622082
yh1008 <ye.hua@columbia.edu>
2017-04-15 00:31:53 -0400

    add language data prep

--- codeswitch/local/data_prep.sh
+++ codeswitch/local/data_prep.sh
@@ -32,3 +32,15 @@ utils/validate_data_dir.sh --no-feats data/train
 utils/validate_data_dir.sh --no-feats data/test
 
 echo "Acoustic Data preparation succeeded"
+
+# Create words.txt (word symbol table) in data/train and data/test
+for x in train test; do 
+  cut -d ' ' -f 2- data/$x/text | sed 's/ /\n/g' | sort -u > data/$x/words.txt
+done
+
+# Create nnsilence_phones.txt and optional_silence.txt
+cut -d ' ' -f 2- data/local/lang/lexicon.txt | sed 's/ /\n/g' | sort -u > data/local/lang/nonsilence_phones.txt
+echo 'SIL' > data/local/lang/optional_silence.txt
+
+utils/prepare_lang.sh data/local/lang '<oov>' data/local/ data/lang
+echo "Language Data preparation succeeded"
