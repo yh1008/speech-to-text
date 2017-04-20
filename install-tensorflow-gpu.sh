8bdf00e2da5838022def64a8cf803b9e0e1d7899
yh1008 <ye.hua@columbia.edu>
2017-04-20 13:34:12 -0400

    update install whl

--- install-tensorflow-gpu.sh
+++ install-tensorflow-gpu.sh
@@ -31,4 +31,5 @@ cd tensorflow
 bazel build -c opt //tensorflow/tools/pip_package:build_pip_package
 bazel build -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 bazel-bin/tensorflow/tools/pip_package/build_pip_package /tmp/tensorflow_pkg
-sudo pip install /tmp/tensorflow_pkg/tensorflow-0.11.0-py2-none-any.whl
+sudo pip install /tmp/tensorflow_pkg/tensorflow-1.1.0rc2-cp27-cp27mu-linux_x86_64.whl
+# depends on distribution on your system, to check which .whl it is, type ls -l /tmp/tensorflow_pkg
